c2a922a2dbefd9fdc804714c923cd5dc87913c7d
Mayur <m.r.rawte7@gmail.com>
2017-09-20 15:06:26 +0530

    Responsive fix

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UtilService} from '../util.service';
+import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-side-nav',
@@ -8,10 +9,21 @@ import {UtilService} from '../util.service';
 })
 export class SideNavComponent implements OnInit, OnDestroy{
   sideNavIsHidden = true;
-  constructor(private utilService: UtilService) { }
+  isatHome = false;
+  constructor(private utilService: UtilService, private router: Router) { }
   ngOnInit() {
     this.utilService.sideNav.subscribe((res: boolean) => {
       this.onToggle();
+    });
+    this.router.events.subscribe((data) => {
+      if (data instanceof NavigationEnd) {
+        console.log(data.url);
+        if (data.url === '/') {
+          this.isatHome = true;
+        } else {
+          this.isatHome = false;
+        }
+      }
     });
   }
   onToggle() {
